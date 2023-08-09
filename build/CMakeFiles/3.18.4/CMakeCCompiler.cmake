0d423cdf35a74f337abcfb5b7c6caadb8967753c
Ken Thompson <kenthompson1@gmail.com>
2023-08-09 18:57:01 +0000

    more dev

--- build/CMakeFiles/3.18.4/CMakeCCompiler.cmake
+++ build/CMakeFiles/3.18.4/CMakeCCompiler.cmake
@@ -1,7 +1,11 @@
+<<<<<<< HEAD
 set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc")
+=======
+set(CMAKE_C_COMPILER "/usr/bin/clang")
+>>>>>>> 62dd8bd (more dev)
 set(CMAKE_C_COMPILER_ARG1 "")
-set(CMAKE_C_COMPILER_ID "GNU")
-set(CMAKE_C_COMPILER_VERSION "10.2.1")
+set(CMAKE_C_COMPILER_ID "Clang")
+set(CMAKE_C_COMPILER_VERSION "11.0.1")
 set(CMAKE_C_COMPILER_VERSION_INTERNAL "")
 set(CMAKE_C_COMPILER_WRAPPER "")
 set(CMAKE_C_STANDARD_COMPUTED_DEFAULT "11")
@@ -12,19 +16,27 @@ set(CMAKE_C11_COMPILE_FEATURES "c_std_11;c_static_assert")
 
 set(CMAKE_C_PLATFORM_ID "Linux")
 set(CMAKE_C_SIMULATE_ID "")
-set(CMAKE_C_COMPILER_FRONTEND_VARIANT "")
+set(CMAKE_C_COMPILER_FRONTEND_VARIANT "GNU")
 set(CMAKE_C_SIMULATE_VERSION "")
 
 
 
 
+<<<<<<< HEAD
 set(CMAKE_AR "/usr/bin/aarch64-linux-gnu-ar")
 set(CMAKE_C_COMPILER_AR "/usr/bin/aarch64-linux-gnu-gcc-ar-10")
 set(CMAKE_RANLIB "/usr/bin/aarch64-linux-gnu-ranlib")
 set(CMAKE_C_COMPILER_RANLIB "/usr/bin/aarch64-linux-gnu-gcc-ranlib-10")
 set(CMAKE_LINKER "/usr/bin/aarch64-linux-gnu-ld")
+=======
+set(CMAKE_AR "/usr/bin/ar")
+set(CMAKE_C_COMPILER_AR "/usr/lib/llvm-11/bin/llvm-ar")
+set(CMAKE_RANLIB "/usr/bin/ranlib")
+set(CMAKE_C_COMPILER_RANLIB "/usr/lib/llvm-11/bin/llvm-ranlib")
+set(CMAKE_LINKER "/usr/bin/ld")
+>>>>>>> 62dd8bd (more dev)
 set(CMAKE_MT "")
-set(CMAKE_COMPILER_IS_GNUCC 1)
+set(CMAKE_COMPILER_IS_GNUCC )
 set(CMAKE_C_COMPILER_LOADED 1)
 set(CMAKE_C_COMPILER_WORKS TRUE)
 set(CMAKE_C_ABI_COMPILED TRUE)
@@ -71,7 +83,13 @@ endif()
 
 
 
+<<<<<<< HEAD
 set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "/usr/lib/gcc/aarch64-linux-gnu/10/include;/usr/local/include;/usr/include/aarch64-linux-gnu;/usr/include")
 set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "gcc;gcc_s;c;gcc;gcc_s")
 set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "/usr/lib/gcc/aarch64-linux-gnu/10;/usr/lib/aarch64-linux-gnu;/usr/lib;/lib/aarch64-linux-gnu;/lib")
+=======
+set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "/usr/local/include;/usr/lib/llvm-11/lib/clang/11.0.1/include;/usr/include/aarch64-linux-gnu;/usr/include")
+set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "gcc;gcc_s;c;gcc;gcc_s")
+set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "/usr/lib/gcc/aarch64-linux-gnu/10;/usr/lib/aarch64-linux-gnu;/lib/aarch64-linux-gnu;/usr/lib;/usr/lib/llvm-11/lib;/lib")
+>>>>>>> 62dd8bd (more dev)
 set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
